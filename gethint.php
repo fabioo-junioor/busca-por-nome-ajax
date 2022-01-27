e99e9f2e659cb7efdf62bbd4fb0bcf1bc5d250a9
FabioJr <fj_011@hotmail.com>
2022-01-27 13:34:11 -0300

    Descrição no README.me

---
+++ gethint.php
@@ -0,0 +1,30 @@
+<?php
+echo "OLA";
+$a = ["Maria, José, Antônio, João, Francisco, Ana, Luiz, Paulo,
+        Carlos, Manoel, Pedro, Francisca, Marcos, Raimundo, Sebastião,
+        Antônia, Marcelo, Jorge, Márcia, Geraldo, Adriana, Sandra, 
+        Luis, Fernando, Fabio, Roberto, Márcio, Edson, André, Sérgio,
+        Josefa, Patrícia, Daniel, Rodrigo, Rafael, Joaquim, Vera, 
+        Ricardo, Eduardo, Terezinha, Sônia, Alexandre, Rita, Luciana,
+        Cláudio, Rosa, Benedito, Leandro, Raimunda, Mário"];
+
+$q = $_GET["q"];
+$hint = "";
+
+if ($q !== "") {
+    $q = strtolower($q);
+    $len = strlen($q);
+    foreach($a as $name){
+        if(stristr($q, substr($name, 0, $len))){
+            if($hint === ""){
+                $hint = $name;
+
+            }else{
+                $hint .= ", $name";
+
+            }
+        }
+    }
+}
+echo $hint === "" ? "Sem sugestão" :$hint;
+?>
